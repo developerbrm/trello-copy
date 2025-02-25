772eb09ea82cbfc6a4167d06046a3407929e0c44
developerbrm <developerbrm@gmail.com>
2025-02-25 14:18:08 +0530

    - tailwind css classes sort added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App = () => {
-  return <div className="p-10  h-20 bg-sky-700"> App</div>;
-};
+  return <div className="m-2 h-20 bg-sky-600 p-5"> App</div>
+}
 
-export default App;
+export default App
