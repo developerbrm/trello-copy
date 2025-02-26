2ca54087ec4f2cfe62a0147ba28d411b0cd21fb0
developerbrm <developerbrm@gmail.com>
2025-02-26 13:20:56 +0530

    - bg container added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import TodoContainer from './components/TodoContainer'
 
 const App = () => {
-  return <TodoContainer />
+  return (
+    <main className="relative">
+      <TodoContainer />
+
+      <div className="bg-container"></div>
+    </main>
+  )
 }
 
 export default App
