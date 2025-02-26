feb8211398e7cf9335d85e8bc844d3e1c5212515
developerbrm <developerbrm@gmail.com>
2025-02-26 22:50:20 +0530

    - ui changes

--- src/components/TodoSection.tsx
+++ src/components/TodoSection.tsx
@@ -1,45 +1,12 @@
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { useContext } from 'react'
-import { TodoItem, TodosMapInterface, TodoStatus } from '../models/Todo.model'
+import { TodoStatus } from '../models/Todo.model'
 import { DragAndDropContext } from '../Providers/DragAndDropProvider'
+import { getSectionData } from '../utilities'
 import Todo from './Todo'
 
 interface ITodoSection {
   status: TodoStatus
-}
-
-interface ISectionData {
-  [key: string]: {
-    todos: TodoItem[]
-    heading: string
-    sectionClassName: string
-    todoClassName: string
-  }
-}
-
-const getSectionData = (status: TodoStatus, todosMap: TodosMapInterface) => {
-  const obj: ISectionData = {
-    pending: {
-      todos: todosMap.get('pending') ?? [],
-      heading: 'Pending',
-      sectionClassName: 'bg-orange-500',
-      todoClassName: 'bg-orange-200 text-orange-950',
-    },
-    completed: {
-      todos: todosMap.get('completed') ?? [],
-      heading: 'Completed',
-      sectionClassName: 'bg-green-500',
-      todoClassName: 'bg-green-200 text-green-950',
-    },
-    'in-progress': {
-      todos: todosMap.get('in-progress') ?? [],
-      heading: 'In Progress',
-      sectionClassName: 'bg-blue-500',
-      todoClassName: 'bg-blue-200 text-blue-950',
-    },
-  }
-
-  return obj[status]
 }
 
 const TodoSection = (props: ITodoSection) => {
@@ -53,11 +20,13 @@ const TodoSection = (props: ITodoSection) => {
 
   return (
     <SortableContext items={todos} strategy={verticalListSortingStrategy}>
-      <div className={`${sectionClassName} rounded-lg p-3 shadow`}>
-        <h3 className="my-2 mb-4 text-2xl font-bold text-white">
+      <div
+        className={`${sectionClassName} rounded-lg bg-gradient-to-br p-3 shadow`}
+      >
+        <h3 className="my-2 mb-4 text-2xl font-bold text-white/90">
           <span>{heading}</span>
 
-          <hr className="my-2 rounded-full border-0 ring ring-white/80" />
+          <hr className="my-2 rounded-full border-0 ring ring-white/50" />
         </h3>
 
         <div className="grid gap-4">
--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,3 +1,5 @@
+import { TodoItem, TodosMapInterface, TodoStatus } from '../models/Todo.model'
+
 export type UiState = {
   data: unknown
   loading: boolean
@@ -9,3 +11,40 @@ export const createInitialState = (): UiState => ({
   loading: false,
   error: null,
 })
+
+interface ISectionData {
+  [key: string]: {
+    todos: TodoItem[]
+    heading: string
+    sectionClassName: string
+    todoClassName: string
+  }
+}
+
+export const getSectionData = (
+  status: TodoStatus,
+  todosMap: TodosMapInterface
+) => {
+  const obj: ISectionData = {
+    pending: {
+      todos: todosMap.get('pending') ?? [],
+      heading: 'Pending',
+      sectionClassName: 'from-orange-500 via-orange-600 to-orange-800',
+      todoClassName: 'bg-orange-200 text-orange-900',
+    },
+    completed: {
+      todos: todosMap.get('completed') ?? [],
+      heading: 'Completed',
+      sectionClassName: 'from-emerald-500 via-emerald-600 to-emerald-800',
+      todoClassName: 'bg-emerald-200 text-emerald-900',
+    },
+    'in-progress': {
+      todos: todosMap.get('in-progress') ?? [],
+      heading: 'In Progress',
+      sectionClassName: 'from-indigo-500 via-indigo-600 to-indigo-800',
+      todoClassName: 'bg-indigo-200 text-indigo-900',
+    },
+  }
+
+  return obj[status]
+}
