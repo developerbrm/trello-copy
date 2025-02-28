abc0786d93f815c1380ab9844fdfd897a6281227
developerbrm <developerbrm@gmail.com>
2025-02-28 22:00:45 +0530

    - ui fixes

--- src/components/UpdateTodoModal.tsx
+++ src/components/UpdateTodoModal.tsx
@@ -96,7 +96,7 @@ const UpdateTodoModal = (props: IUpdateTodoModal) => {
         className={`${showUpdateTodoModal ? '' : 'pointer-events-none opacity-0'} fixed inset-0 z-10 w-screen overflow-y-auto transition-opacity`}
       >
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl ring ring-zinc-300 transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="relative w-full max-w-lg transform overflow-hidden rounded-lg bg-white text-left shadow-xl ring ring-zinc-300 transition-all sm:my-8">
             <form onSubmit={handleUpdate}>
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
